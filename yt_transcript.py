90524a0bd0b9300e4acca6991c07e4d708da30c3
vohrarishit <vohrarishit@gmail.com>
2026-06-14 09:36:38 +0530

    Fix transcript extraction

--- yt_transcript.py
+++ yt_transcript.py
@@ -2,10 +2,18 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 def get_transcript(video_id):
 
-    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    try:
+        transcript = YouTubeTranscriptApi.get_transcript(
+            video_id,
+            languages=['en']
+        )
 
-    text = " ".join(
-        item["text"] for item in transcript
-    )
+        text = " ".join(
+            item["text"] for item in transcript
+        )
 
-    return text
+        return text
+
+    except Exception as e:
+        print(f"Transcript Error: {e}")
+        return None
